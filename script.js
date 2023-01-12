a99c7470623680a4018f0e858830ac92dc853aa5
arthursl12 <arthursoutolima@gmail.com>
2023-01-12 08:08:23 -0300

    Create getComputerChoice

--- script.js
+++ script.js
@@ -1,1 +1,25 @@
 console.log("Hello")
+
+
+function getComputerChoice(){
+    // Random number between 1 and 3
+    let choice = (Math.floor(Math.random() * 3))+1; 
+    console.log(choice)
+    switch (choice) {
+        case 1:
+            return "Rock"
+            break;
+        case 2:
+            return "Paper"
+            break;
+        case 3:
+            return "Scissors"
+            break;
+        default:
+            alert("Unknown choice")
+            break;
+    }
+} 
+
+
+console.log(getComputerChoice())
